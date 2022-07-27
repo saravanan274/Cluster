517f7921cb5ef5c7edfd60f769664f9f870be1a4
sajinvs <105791378+sajinvs@users.noreply.github.com>
2022-07-27 15:05:32 +0530

    new files

---
+++ Cluster/header.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <!-- ==============================================
+        Basic Page Needs
+        =============================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+
+        <title>CLUSTERUAE - Business, Consulting, Finance, Insurance, Startup and Technology</title>
+
+        <meta name="description" content="Business, Consulting, Finance, Insurance, Startup and Technology">
+        <meta name="subject" content="Business, Consulting, Finance, Insurance, Startup and Technology">
+        <meta name="author" content="Xetnica">
+
+        <!-- ==============================================
+        Favicons
+        =============================================== -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+
+        <!-- ==============================================
+        Vendor Stylesheet
+        =============================================== -->
+        <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/slider.min.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/vendor/icons.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/icons-fa.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/animation.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/gallery.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/cookie-notice.min.css">
+
+        <!-- ==============================================
+        Custom Stylesheet
+        =============================================== -->
+        <link rel="stylesheet" href="assets/css/default.css">
+        <link rel="stylesheet" href="assets/css/cluster.css">
+
+        <!-- ==============================================
+        Theme Color
+        =============================================== -->
+        <meta name="theme-color" content="#21333e">
+
+        <!-- ==============================================
+        Theme Settings
+        =============================================== -->
+        <style>
+            
+            :root {
+                --hero-bg-color: #080d10;
+
+                --section-1-bg-color: #eef4ed;
+                --section-2-bg-color: #ffffff;
+                --section-3-bg-color: #111117;
+                --section-4-bg-color: #ffffff;
+                --section-5-bg-color: #eef4ed;
+                --section-6-bg-color: #111117;
+            }
+        </style>
+        
+    </head>
+
+    <!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+
+        <!-- ==============================================
+        Basic Page Needs
+        =============================================== -->
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+
+        <title>CLUSTERUAE - Business, Consulting, Finance, Insurance, Startup and Technology</title>
+
+        <meta name="description" content="Business, Consulting, Finance, Insurance, Startup and Technology">
+        <meta name="subject" content="Business, Consulting, Finance, Insurance, Startup and Technology">
+        <meta name="author" content="Xetnica">
+
+        <!-- ==============================================
+        Favicons
+        =============================================== -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+
+        <!-- ==============================================
+        Vendor Stylesheet
+        =============================================== -->
+        <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/slider.min.css">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/vendor/icons.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/icons-fa.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/animation.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/gallery.min.css">
+        <link rel="stylesheet" href="assets/css/vendor/cookie-notice.min.css">
+
+        <!-- ==============================================
+        Custom Stylesheet
+        =============================================== -->
+        <link rel="stylesheet" href="assets/css/default.css">
+
+        <!-- ==============================================
+        Theme Color
+        =============================================== -->
+        <meta name="theme-color" content="#21333e">
+
+        <!-- ==============================================
+        Theme Settings
+        =============================================== -->
+        <style>
+            :root {
+                --hero-bg-color: #080d10;
+
+                --section-1-bg-color: #eef4ed;
+                --section-2-bg-color: #ffffff;
+                --section-3-bg-color: #111117;
+                --section-4-bg-color: #ffffff;
+                --section-5-bg-color: #eef4ed;
+                --section-6-bg-color: #111117;
+            }
+        </style>
+        
+    </head>
+
+    <body class="home">
+        
+        <!-- Preloader -->
+        <div id="preloader" data-timeout="2000" class="odd preloader counter">
+            <div data-aos="fade-up" data-aos-delay="500" class="row justify-content-center text-center items">
+                <div data-percent="100" class="radial">
+                    <span></span>
+                </div>
+            </div>
+        </div>
+
+        <!-- Header -->
+        <header id="header">
+
+            <!-- Top Navbar -->
+            <nav class="navbar navbar-expand top">
+                <div class="container header">
+
+                    <!-- Navbar Items [left] -->
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link pl-0"><i class="fas fa-clock mr-2"></i>Open Hours: Mon - Sat - 9:00 - 18:00</a>
+                        </li>
+                    </ul>
+
+                    <!-- Nav holder -->
+                    <div class="ml-auto"></div>
+
+                    <!-- Navbar Items [right] -->
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"><i class="fas fa-phone-alt mr-2"></i>+1 (305) 1234-5678</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link"><i class="fas fa-envelope mr-2"></i><span class="__cf_email__" data-cfemail="335b565f5f5c73564b525e435f561d505c5e">[email&#160;protected]</span></a>
+                        </li>
+                    </ul>
+
+                    <!-- Navbar Icons -->
+                    <ul class="navbar-nav icons">
+                        <li class="nav-item social">
+                            <a href="#" class="nav-link"><i class="fab fa-facebook-f"></i></a>
+                        </li>
+                        <li class="nav-item social">
+                            <a href="#" class="nav-link"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li class="nav-item social">
+                            <a href="#" class="nav-link pr-0"><i class="fab fa-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav>
+
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand navbar-fixed sub" style="background-color: black;">
+                <div class="container header">
+
+                    <!-- Navbar Brand-->
+                    <a class="navbar-brand" href="#">
+                        <span class="brand">
+                           <img src ="assets/images/logo_thumb.png" alt ="CLUSTERUAE" width="200px" height="200px">
+                        
+                        <!-- 
+                            Custom Logo
+                            <img src="assets/images/logo.svg" alt="CLUSTERUAE">
+                        -->
+                    </a>
+
+                    <!-- Nav holder -->
+                    <div class="ml-auto"></div>
+
+                    <!-- Navbar Items -->
+                    <ul class="navbar-nav items">
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link">HOME </a>
+                            
+                    </li>
+                        <li class="nav-item dropdown">
+                            <a href="itservices.php" class="nav-link">IT SERVICES <i class="icon-arrow-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item dropdown"><a class="nav-link" href="#">Cloud Solutions</a>
+                                </li>
+                                <li class="nav-item"><a href="page-services-1.html" class="nav-link">IT Project Management</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link" href="#">IT Security</a>
+                                </li>
+                                <li class="nav-item"><a href="page-projects-1.html" class="nav-link">IT Support Services</a></li>
+                                <li class="nav-item"><a href="page-single-project-1.html" class="nav-link"> Smarts Hands Support Service</a></li>
+                                <li class="nav-item"><a href="page-pricing-table-1.html" class="nav-link">Telephony</a></li>
+                                <li class="nav-item"><a href="page-our-people-1.html" class="nav-link">Turnkey Office Fit out</a></li>
+                            </ul>
+                    </li>
+                        <li class="nav-item dropdown">
+                            <a href="itsolution.html" class="nav-link"> IT SOLUTIONS <i class="icon-arrow-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link" href="#"> Business Continuity & Disaster Recovery </a></li>
+                                <li class="nav-item"><a href="page-single-post-1.html" class="nav-link">Business Continuity Management</a></li>
+                                <li class="nav-item"><a href="page-projects-1.html" class="nav-link">Collaboration</a></li>
+                                <li class="nav-item"><a href="page-single-project-1.html" class="nav-link"> Data Protection and Recovery</a></li>
+                                <li class="nav-item"><a href="page-pricing-table-1.html" class="nav-link">Desktop as a Service (DaaS)</a></li>
+                                <li class="nav-item"><a href="page-our-people-1.html" class="nav-link">Endpoint Monitoring and Management</a></li>
+                                <li class="nav-item"><a href="page-projects-1.html" class="nav-link">Enterprise File Sync & Share</a></li>
+                                <li class="nav-item"><a href="page-single-project-1.html" class="nav-link"> Infrastructure Design and Deployment</a></li>
+                                <li class="nav-item"><a href="page-pricing-table-1.html" class="nav-link">Managed Security</a></li>
+                                <li class="nav-item"><a href="page-our-people-1.html" class="nav-link">Managed Security</a></li>
+                                <li class="nav-item"><a href="page-our-people-1.html" class="nav-link">Virtualization</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="menu/partnerproduct.html" class="nav-link">PARTNERS & PRODUCTS</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="menu/partnerproduct.html" class="nav-link">CLIENTS <i class="icon-arrow-down"></i></a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item dropdown">
+                                <li class="nav-item"><a class="nav-link" href="#"> Testimonials </a></li>
+                                <li class="nav-item"><a href="page-single-post-1.html" class="nav-link">Our Clients</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="menu/partnerproduct.html" class="nav-link">BLOG</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="menu/partnerproduct.html" class="nav-link">ABOUT US</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a href="menu/partnerproduct.html" class="nav-link">CONTACT US</a>
+                        </li>
+                    </ul>
+
+                    <!-- Navbar Icons -->
+                    <ul class="navbar-nav icons">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" data-toggle="modal" data-target="#search">
+                                <i class="icon-magnifier"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" data-toggle="modal" data-target="#sign">
+                                <i class="icon-user"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <!-- Navbar Toggle -->
+                    <ul class="navbar-nav toggle">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link" data-toggle="modal" data-target="#menu">
+                                <i class="icon-menu m-0"></i>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <!-- Navbar Action -->
+                    
+                </div>
+            </nav>
+
+        </header>
